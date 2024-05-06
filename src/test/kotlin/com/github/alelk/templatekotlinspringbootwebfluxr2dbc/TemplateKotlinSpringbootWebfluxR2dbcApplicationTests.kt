724848a0aea44fdb3e12ff9118da0b194c9feae5
Alex Elkin <elckinne@gmail.com>
2024-05-06 14:45:54 +0300

    build: init empty spring boot project (using spring initializr)

---
+++ src/test/kotlin/com/github/alelk/templatekotlinspringbootwebfluxr2dbc/TemplateKotlinSpringbootWebfluxR2dbcApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.alelk.templatekotlinspringbootwebfluxr2dbc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TemplateKotlinSpringbootWebfluxR2dbcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
