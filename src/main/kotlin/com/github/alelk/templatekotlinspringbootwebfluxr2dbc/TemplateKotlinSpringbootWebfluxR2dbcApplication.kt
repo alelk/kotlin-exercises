8e340bcff50424b3ac2b1f08c5adc629e3a171b9
Alex Elkin <alelkdev@gmail.com>
2024-05-06 14:45:54 +0300

    build: init empty spring boot project (using spring initializr)

---
+++ src/main/kotlin/com/github/alelk/templatekotlinspringbootwebfluxr2dbc/TemplateKotlinSpringbootWebfluxR2dbcApplication.kt
@@ -0,0 +1,11 @@
+package com.github.alelk.templatekotlinspringbootwebfluxr2dbc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TemplateKotlinSpringbootWebfluxR2dbcApplication
+
+fun main(args: Array<String>) {
+	runApplication<TemplateKotlinSpringbootWebfluxR2dbcApplication>(*args)
+}
