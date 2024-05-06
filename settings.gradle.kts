b32f5a3e86f92c8fc7a5a66b9c751b8e5f2cf688
Alex Elkin <alelkdev@gmail.com>
2024-05-06 14:56:16 +0300

    build: configure library versions file

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,9 @@
 rootProject.name = "template-kotlin-springboot-webflux-r2dbc"
+
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("libs") {
+            from(files("libs.versions.toml"))
+        }
+    }
+}
