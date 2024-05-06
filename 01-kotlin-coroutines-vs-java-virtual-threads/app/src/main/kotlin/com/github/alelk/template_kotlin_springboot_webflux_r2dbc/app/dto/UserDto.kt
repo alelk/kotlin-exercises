3fdea86130732aa420eff32358494d0ce2ba8742
Alex Elkin <alelkdev@gmail.com>
2024-05-06 16:54:46 +0300

    feat(01): implement user activity service

--- 01-kotlin-coroutines-vs-java-virtual-threads/app/src/main/kotlin/com/github/alelk/template_kotlin_springboot_webflux_r2dbc/app/dto/UserDto.kt
+++
@@ -1,8 +0,0 @@
-package com.github.alelk.template_kotlin_springboot_webflux_r2dbc.app.dto
-
-data class UserDto(
-  val id: Long?,
-  val name: String,
-  val email: String,
-  val phone: String
-)
--- 01-kotlin-coroutines-vs-java-virtual-threads/user-activity-service/src/main/kotlin/com/github/alelk/template_kotlin_springboot_webflux_r2dbc/app/UserActivityServiceStartup.kt
+++ 01-kotlin-coroutines-vs-java-virtual-threads/user-activity-service/src/main/kotlin/com/github/alelk/template_kotlin_springboot_webflux_r2dbc/app/UserActivityServiceStartup.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class AppStartup
+class UserActivityServiceStartup
 
 fun main(args: Array<String>) {
-	runApplication<AppStartup>(*args)
+  runApplication<UserActivityServiceStartup>(*args)
 }
