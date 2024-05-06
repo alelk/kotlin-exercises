d4d4e7bf2122ea124afd71dc4f0d814619f2693b
Alex Elkin <elckinne@gmail.com>
2024-05-06 14:56:16 +0300

    build: configure library versions file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
-	kotlin("jvm") version "1.9.23"
-	kotlin("plugin.spring") version "1.9.23"
+	alias(libs.plugins.springBoot)
+	alias(libs.plugins.spring.dependencyManagement)
+	alias(libs.plugins.kotlin.jvm)
+	alias(libs.plugins.kotlin.spring)
 }
 
 group = "com.github.alelk"
